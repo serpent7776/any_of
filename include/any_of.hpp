5cfd95469413d57cd49ccd4745f99bbe307118e6
Serpent7776 <serpent7776@gmail.com>
2021-05-03 15:57:21 +0200

    Implemented any_of(...) == value

--- include/any_of.hpp
+++ include/any_of.hpp
@@ -1,5 +1,42 @@
+#include <utility>
+
 template <typename T, typename ...Ts>
 bool equals_to_any_of(T val, Ts... vals)
 {
 	return ((val == vals) || ...);
+}
+
+template <size_t, typename T>
+class Value
+{
+public:
+	Value(T&& value) : value {std::forward<T>(value)}
+	{}
+
+	const T& get() const { return value; }
+
+private:
+	T value;
+};
+
+template <class Idxs, class ...Ts>
+struct Values;
+
+template <size_t ...Idxs, typename ...Ts>
+struct Values<std::index_sequence<Idxs...>, Ts...> : Value<Idxs, Ts>...
+{
+	Values(Ts&&... vs) : Value<Idxs, Ts>(std::forward<Ts>(vs))...
+	{}
+
+	template <typename T>
+	bool operator==(T&& value) const
+	{
+		return ((value == Value<Idxs, Ts>::get()) || ...);
+	}
+};
+
+template <typename ...Ts>
+auto any_of(Ts&&... vals)
+{
+	return Values<std::index_sequence_for<Ts...>, Ts...> (std::forward<Ts>(vals)...);
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -24,3 +24,27 @@ TEST_CASE("equals_to_any_of for strings")
 	const bool result = equals_to_any_of("foo", "baz", "bar", "foo");
 	REQUIRE(result);
 }
+
+TEST_CASE("any_of == finds first value")
+{
+	const bool result = any_of(1, 2, 3) == 1;
+	REQUIRE(result);
+}
+
+TEST_CASE("any_of == finds last value")
+{
+	const bool result = any_of(1, 2, 3) == 3;
+	REQUIRE(result);
+}
+
+TEST_CASE("any_of == returns false if value not found")
+{
+	const bool result = any_of(1, 2, 3) == 9;
+	REQUIRE(not result);
+}
+
+TEST_CASE("any_of == for strings")
+{
+	const bool result = any_of("foo", "bar", "baz") == "foo";
+	REQUIRE(result);
+}
