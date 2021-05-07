2ee4ac289a225b79434b990478e3e1f07ec48c76
Serpent7776 <serpent7776@gmail.com>
2021-05-07 18:28:06 +0200

    Use forwarding references

--- include/any_of.hpp
+++ include/any_of.hpp
@@ -1,7 +1,7 @@
 #include <utility>
 
 template <typename T, typename ...Ts>
-bool equals_to_any_of(T val, Ts... vals)
+bool equals_to_any_of(T&& val, Ts&&... vals)
 {
 	return ((val == vals) || ...);
 }
