80f29654111d3ea59d377a5975e1923c29c7e394
Serpent7776 <serpent7776@gmail.com>
2021-06-13 16:53:26 +0200

    Split unit tests to multiple files

---
+++ test/unit/any_of_basic_tests.cpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <type_traits>
+
+#include <catch2/catch.hpp>
+#include <any_of.hpp>
+
+static_assert(std::is_aggregate_v<decltype(srp::any_of(12))>);
+static_assert(std::is_aggregate_v<srp::Value<0, int>>);
+
+TEST_CASE("any_of == finds first value")
+{
+	const bool result = srp::any_of(1, 2, 3) == 1;
+	REQUIRE(result);
+}
+
+TEST_CASE("any_of == finds last value")
+{
+	const bool result = srp::any_of(1, 2, 3) == 3;
+	REQUIRE(result);
+}
+
+TEST_CASE("any_of == returns false if value not found")
+{
+	const bool result = srp::any_of(1, 2, 3) == 9;
+	REQUIRE(not result);
+}
+
+TEST_CASE("any_of == for strings")
+{
+	const bool result = srp::any_of("foo", "bar", "baz") == "foo";
+	REQUIRE(result);
+}
+
+TEST_CASE("== any_of finds first value")
+{
+	const bool result = 1 == srp::any_of(1, 2, 3);
+	REQUIRE(result);
+}
+
+TEST_CASE("== any_of finds last value")
+{
+	const bool result = 3 == srp::any_of(1, 2, 3);
+	REQUIRE(result);
+}
+
+TEST_CASE("== any_of returns false if value not found")
+{
+	const bool result = 9 == srp::any_of(1, 2, 3);
+	REQUIRE(not result);
+}
+
+TEST_CASE("== any_of for strings")
+{
+	const bool result = "foo" == srp::any_of("foo", "bar", "baz");
+	REQUIRE(result);
+}
