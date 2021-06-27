ad82fe0b078eafef46730882dbfd599b2ac53a3f
Serpent7776 <serpent7776@gmail.com>
2021-06-27 13:44:50 +0200

    Extract EqOp

--- include/any_of.hpp
+++ include/any_of.hpp
@@ -43,31 +43,39 @@ struct Value
 	const T& value;
 };
 
-template <class ReduceOp, class EqOp, class Idxs, class ...Ts>
-struct Pack;
-
-template <class ReduceOp, class EqOp, std::size_t ...Idxs, typename ...Ts>
-struct Pack<ReduceOp, EqOp, std::index_sequence<Idxs...>, Ts...> : Value<Idxs, Ts>...
+template <class Reducer, class Eq>
+struct EqOp
 {
-	template <typename T>
-	friend bool __attribute__((always_inline)) operator==(const Pack& pack, const T& value)
+	[[no_unique_address]] Reducer reducer;
+	[[no_unique_address]] Eq eq;
+
+	template <template <typename, typename, typename, typename...> class Pack, typename T, size_t ...Idxs, typename ...Ts>
+	friend bool __attribute__((always_inline)) operator==(const Pack<Reducer, Eq, std::index_sequence<Idxs...>, Ts...>& pack, const T& value)
 	{
-		const auto eq = EqOp();
-		const auto reduce = ReduceOp();
-		return reduce(eq(value, pack.Value<Idxs, Ts>::value)...);
+		const Reducer& reducer = pack.EqOp<Reducer, Eq>::reducer;
+		const Eq& eq = pack.EqOp<Reducer, Eq>::eq;
+		return reducer(eq(value, pack.Value<Idxs, Ts>::value)...);
 	}
 
-	template <typename T>
-	friend bool __attribute__((always_inline)) operator==(const T& value, const Pack& pack)
+	template <template <typename, typename, typename, typename...> class Pack, typename T, size_t ...Idxs, typename ...Ts>
+	friend bool __attribute__((always_inline)) operator==(const T& value, const Pack<Reducer, Eq, std::index_sequence<Idxs...>, Ts...>& pack)
 	{
 		return pack == value;
 	}
 };
 
+template <class Reducer, class Eq, class Idxs, class ...Ts>
+struct Pack;
+
+template <class Reducer, class Eq, std::size_t ...Idxs, typename ...Ts>
+struct Pack<Reducer, Eq, std::index_sequence<Idxs...>, Ts...> : EqOp<Reducer, Eq>, Value<Idxs, Ts>...
+{
+};
+
 template <typename ReduceOp, typename EqOp, typename ...Ts>
 auto make_pack(const Ts&... values)
 {
-	return Pack<ReduceOp, EqOp, std::index_sequence_for<Ts...>, Ts...> {{values}...};
+	return Pack<ReduceOp, EqOp, std::index_sequence_for<Ts...>, Ts...> {{}, {values}...};
 }
 
 template <typename ...Ts>
