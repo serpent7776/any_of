a0a5e8966ac0138185312e2101890d1021337d98
Serpent7776 <serpent7776@gmail.com>
2021-06-30 19:58:20 +0200

    Make operator list configurable
    Remove direct dependency between Pack<> and EqOp<>. Instead of that,
    Pack<> takes a list of classes that define allowed operators.
    With that change we can now create custom pack objects with any operator
    overloaded, e.g.
    
      make_pack<EqOp<logical_or, std::equal_to<void>>>(vals...)
    
    will create any_of will only `operator==` defined, whereas
    
      make_pack<
        EqOp<logical_or, std::equal_to<void>>,
        NeqOp<logical_or, std::not_equal_to<void>>>(vals...)
    
    will create any_of with both `operator==` and `operator!=` defined.

--- include/any_of.hpp
+++ include/any_of.hpp
@@ -49,57 +49,60 @@ struct EqOp
 	[[no_unique_address]] Reducer reducer;
 	[[no_unique_address]] Eq eq;
 
-	template <template <typename, typename, typename, typename...> class Pack, typename T, size_t ...Idxs, typename ...Ts>
-	friend bool __attribute__((always_inline)) operator==(const Pack<Reducer, Eq, std::index_sequence<Idxs...>, Ts...>& pack, const T& value)
+	template <template <typename, typename, typename...> class Pack, typename Ops, typename T, size_t ...Idxs, typename ...Ts>
+	friend bool __attribute__((always_inline)) operator==(const Pack<Ops, std::index_sequence<Idxs...>, Ts...>& pack, const T& value)
 	{
 		const Reducer& reducer = pack.EqOp<Reducer, Eq>::reducer;
 		const Eq& eq = pack.EqOp<Reducer, Eq>::eq;
 		return reducer(eq(value, pack.Value<Idxs, Ts>::value)...);
 	}
 
-	template <template <typename, typename, typename, typename...> class Pack, typename T, size_t ...Idxs, typename ...Ts>
-	friend bool __attribute__((always_inline)) operator==(const T& value, const Pack<Reducer, Eq, std::index_sequence<Idxs...>, Ts...>& pack)
+	template <template <typename, typename, typename...> class Pack, typename Ops, typename T, size_t ...Idxs, typename ...Ts>
+	friend bool __attribute__((always_inline)) operator==(const T& value, const Pack<Ops, std::index_sequence<Idxs...>, Ts...>& pack)
 	{
 		return pack == value;
 	}
 };
 
-template <class Reducer, class Eq, class Idxs, class ...Ts>
+template <typename ...Ops>
+struct OpList : Ops... {};
+
+template <class Ops, class Idxs, class ...Ts>
 struct Pack;
 
-template <class Reducer, class Eq, std::size_t ...Idxs, typename ...Ts>
-struct Pack<Reducer, Eq, std::index_sequence<Idxs...>, Ts...> : EqOp<Reducer, Eq>, Value<Idxs, Ts>...
+template <typename ...Ops, std::size_t ...Idxs, typename ...Ts>
+struct Pack<OpList<Ops...>, std::index_sequence<Idxs...>, Ts...> : OpList<Ops...>, Value<Idxs, Ts>...
 {
 };
 
-template <typename ReduceOp, typename EqOp, typename ...Ts>
+template <typename ...Ops, typename ...Ts>
 auto make_pack(const Ts&... values)
 {
-	return Pack<ReduceOp, EqOp, std::index_sequence_for<Ts...>, Ts...> {{}, {values}...};
+	return Pack<OpList<Ops...>, std::index_sequence_for<Ts...>, Ts...> {{}, {values}...};
 }
 
 template <typename ...Ts>
 auto any_of(const Ts&... vals)
 {
-	return make_pack<logical_or, std::equal_to<void>>(vals...);
+	return make_pack<EqOp<logical_or, std::equal_to<void>>>(vals...);
 }
 
 template <typename ...Ts>
 auto none_of(const Ts&... vals)
 {
-	return make_pack<logical_and, std::not_equal_to<void>>(vals...);
+	return make_pack<EqOp<logical_and, std::not_equal_to<void>>>(vals...);
 }
 
 template <typename ...Ts>
 auto all_of(const Ts&... vals)
 {
-	return make_pack<logical_and, std::equal_to<void>> (vals...);
+	return make_pack<EqOp<logical_and, std::equal_to<void>>> (vals...);
 }
 
 template <typename ...Ts>
 auto one_of(const Ts&... vals)
 {
-	return make_pack<only_one, std::equal_to<void>>(vals...);
+	return make_pack<EqOp<only_one, std::equal_to<void>>>(vals...);
 }
 
 }
