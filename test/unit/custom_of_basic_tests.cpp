982aaab3002f4cbad3fab41c366580c27f0e866f
Serpent7776 <serpent7776@gmail.com>
2021-07-24 16:32:48 +0200

    Added test using custom operator

--- test/unit/custom_of_basic_tests.cpp
+++ test/unit/custom_of_basic_tests.cpp
@@ -1,4 +1,4 @@
-#include <string>
+#include <array>
 #include <type_traits>
 
 #include <catch2/catch.hpp>
@@ -15,3 +15,39 @@ TEST_CASE("custom any_of with operators == and !=")
 	REQUIRE(not (custom_any_of(2, 2, 2) != 2));
 }
 
+namespace
+{
+	struct Xor
+	{
+		int operator()(int a, int b) const
+		{
+			return a ^ b;
+		}
+	};
+
+	struct Concat
+	{
+		template <typename ...Ints>
+		std::array<int, sizeof...(Ints)> operator()(Ints... ints) const
+		{
+			return std::array<int, sizeof...(Ints)> {ints...};
+		}
+	};
+
+	struct XorOp : srp::Op<Concat, Xor>
+	{
+		template <typename Pack>
+		friend std::array<int, Pack::size()> operator^(const Pack& pack, int val)
+		{
+			return srp::eval<srp::Op<Concat, Xor>>(pack, val);
+		}
+	};
+}
+
+TEST_CASE("custom_of with custom operator ^")
+{
+	auto xor_of = srp::make_custom_of<XorOp>();
+
+	REQUIRE((xor_of(0, 1, 2, 3) ^ 1) == std::array {1, 0, 3, 2});
+	REQUIRE((xor_of(0, 1, 2, 3, 4, 5) ^ 1) == std::array {1, 0, 3, 2, 5, 4});
+}
