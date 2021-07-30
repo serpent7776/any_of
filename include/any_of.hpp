656676b1939e5331427e368d267428e49ace0c61
Serpent7776 <serpent7776@gmail.com>
2021-07-30 21:26:50 +0200

    Simplified templates
    Get rid of template <> class Pack template params.

--- include/any_of.hpp
+++ include/any_of.hpp
@@ -51,42 +51,54 @@ struct Value
 	const T& value;
 };
 
-template <class Reducer, class Mapper>
+template <typename ...Ops>
+struct OpList : Ops... {};
+
+template <class Ops, class Idxs, class ...Ts>
+struct Pack;
+
+template <typename ...Ops, std::size_t ...Idxs, typename ...Ts>
+struct Pack<OpList<Ops...>, std::index_sequence<Idxs...>, Ts...> : OpList<Ops...>, Value<Idxs, Ts>...
+{
+	constexpr static std::size_t size = sizeof...(Ts);
+};
+
+template <typename Reducer, typename Mapper>
 struct Op
 {
 	[[no_unique_address]] Reducer reduce;
 	[[no_unique_address]] Mapper map;
 
-	template <template <typename, typename, typename...> class Pack, typename Ops, typename T, std::size_t ...Idxs, typename ...Ts>
+	template <typename T, typename Ops, std::size_t ...Idxs, typename ...Ts>
 	auto SRP_ATTR_FORCE_INLINE eval(const Pack<Ops, std::index_sequence<Idxs...>, Ts...>& pack, const T& value) const
 	{
 		return reduce(map(value, pack.Value<Idxs, Ts>::value)...);
 	}
 
-	template <template <typename, typename, typename...> class Pack, typename Ops, typename T, std::size_t ...Idxs, typename ...Ts>
+	template <typename T, typename Ops, std::size_t ...Idxs, typename ...Ts>
 	auto SRP_ATTR_FORCE_INLINE eval(const T& value, const Pack<Ops, std::index_sequence<Idxs...>, Ts...>& pack) const
 	{
 		return eval(pack, value);
 	}
 
-	template <template <typename, typename, typename...> class Pack, typename Ops, std::size_t ...Idxs, typename ...Ts>
+	template <typename Ops, std::size_t ...Idxs, typename ...Ts>
 	auto SRP_ATTR_FORCE_INLINE eval(const Pack<Ops, std::index_sequence<Idxs...>, Ts...>& pack) const
 	{
 		return reduce(map(pack.Value<Idxs, Ts>::value)...);
 	}
 };
 
-template <typename Op, template <typename, typename, typename...> class Pack, typename Ops, typename T, std::size_t ...Idxs, typename ...Ts>
+template <typename Op, typename Ops, typename T, std::size_t ...Idxs, typename ...Ts>
 auto SRP_ATTR_FORCE_INLINE eval(const Pack<Ops, std::index_sequence<Idxs...>, Ts...>& pack, const T& value)
 {
 	return static_cast<const Op&>(pack).eval(pack, value);
 }
-template <typename Op, template <typename, typename, typename...> class Pack, typename Ops, typename T, std::size_t ...Idxs, typename ...Ts>
+template <typename Op, typename Ops, typename T, std::size_t ...Idxs, typename ...Ts>
 auto SRP_ATTR_FORCE_INLINE eval(const T& value, const Pack<Ops, std::index_sequence<Idxs...>, Ts...>& pack)
 {
 	return static_cast<const Op&>(pack).eval(pack, value);
 }
-template <typename Op, template <typename, typename, typename...> class Pack, typename Ops, std::size_t ...Idxs, typename ...Ts>
+template <typename Op, typename Ops, std::size_t ...Idxs, typename ...Ts>
 auto SRP_ATTR_FORCE_INLINE eval(const Pack<Ops, std::index_sequence<Idxs...>, Ts...>& pack)
 {
 	return static_cast<const Op&>(pack).eval(pack);
@@ -98,7 +110,7 @@ struct EqOp : Op<Reducer, Eq>
 	template <typename PackOrValueA, typename PackOrValueB>
 	friend bool SRP_ATTR_FORCE_INLINE operator==(const PackOrValueA& x, const PackOrValueB& y)
 	{
-		return eval<Op<Reducer, Eq>>(x, y);
+		return eval<EqOp>(x, y);
 	}
 };
 
@@ -108,20 +120,8 @@ struct NeqOp : Op<Reducer, Neq>
 	template <typename PackOrValueA, typename PackOrValueB>
 	friend bool SRP_ATTR_FORCE_INLINE operator!=(const PackOrValueA& x, const PackOrValueB& y)
 	{
-		return eval<Op<Reducer, Neq>>(x, y);
+		return eval<NeqOp>(x, y);
 	}
-};
-
-template <typename ...Ops>
-struct OpList : Ops... {};
-
-template <class Ops, class Idxs, class ...Ts>
-struct Pack;
-
-template <typename ...Ops, std::size_t ...Idxs, typename ...Ts>
-struct Pack<OpList<Ops...>, std::index_sequence<Idxs...>, Ts...> : OpList<Ops...>, Value<Idxs, Ts>...
-{
-	constexpr static std::size_t size = sizeof...(Ts);
 };
 
 template <typename ...Ops, typename ...Ts>
